3dc30b56302ad97203be0f339ae59916fa6350c6
Christus Valerian <christus.valerian@inswit.com>
2020-06-19 15:51:28 +0530

    Speech to text

--- test/app/src/main/java/com/christus/release1/admobApp/MainActivity.java
+++ test/app/src/main/java/com/christus/release1/admobApp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.christus.release1.admobApp;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.speech.RecognizerIntent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -22,6 +25,9 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.Locale;
+
 /** Main Activity. Inflates main activity xml. */
 public class MainActivity extends Activity {
     private static final String AD_UNIT_ID = "ca-app-pub-3940256099942544/5224354917";
@@ -36,7 +42,7 @@ public class MainActivity extends Activity {
 
     private RewardedAd rewardedAd;
     private Button retryButton;
-    private Button showVideoButton;
+    private Button showVideoButton, ansPls;
     private long timeRemaining;
     boolean isLoading;
 
@@ -47,6 +53,9 @@ public class MainActivity extends Activity {
     TextView puzzleTitle;
 
     ImageView imageView;
+
+    private static final int REQ_CODE_SPEECH_INPUT = 100;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -105,6 +114,7 @@ public class MainActivity extends Activity {
                 });
 
         // Create the "show" button, which shows a rewarded video if one is loaded.
+        ansPls = findViewById(R.id.ans_pls);
         showVideoButton = findViewById(R.id.show_video_button);
         showVideoButton.setVisibility(View.INVISIBLE);
         showVideoButton.setOnClickListener(
@@ -121,6 +131,25 @@ public class MainActivity extends Activity {
         coinCountText.setText("Coins: " + coinCount);
 
         startGame();
+
+        ansPls.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startVoiceInput();
+            }
+        });
+    }
+
+    private void startVoiceInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hello, How can I help you?");
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+
+        }
     }
 
     private void showCard(JSONObject jsonObject) {
@@ -260,6 +289,24 @@ public class MainActivity extends Activity {
                         }
                     };
             rewardedAd.show(this, adCallback);
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    Toast.makeText(MainActivity.this, result.get(0), Toast.LENGTH_SHORT)
+                            .show();
+                }
+                break;
+            }
+
         }
     }
 }
