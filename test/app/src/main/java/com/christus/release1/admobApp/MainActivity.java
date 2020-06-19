4034e6a94588b11ccbe36e08d6219f9e965b8eb1
Your Name <you@example.com>
2020-06-19 12:34:37 +0530

    First checkin

---
+++ test/app/src/main/java/com/christus/release1/admobApp/MainActivity.java
@@ -0,0 +1,258 @@
+package com.christus.release1.admobApp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.ads.rewarded.RewardItem;
+import com.google.android.gms.ads.rewarded.RewardedAd;
+import com.google.android.gms.ads.rewarded.RewardedAdCallback;
+import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/** Main Activity. Inflates main activity xml. */
+public class MainActivity extends Activity {
+    private static final String AD_UNIT_ID = "ca-app-pub-3940256099942544/5224354917";
+    private static final long COUNTER_TIME = 10;
+    private static final int GAME_OVER_REWARD = 1;
+
+    private int coinCount;
+    private TextView coinCountText;
+    private CountDownTimer countDownTimer;
+    private boolean gameOver;
+    private boolean gamePaused;
+
+    private RewardedAd rewardedAd;
+    private Button retryButton;
+    private Button showVideoButton;
+    private long timeRemaining;
+    boolean isLoading;
+
+    String JSON_STRING = "{\"result\":[{\"title\":\"`’Tis true I have both face and hands,And move before your eyes, Yet when I go, my body stands,And when I stand, I lie.`\",\"imageUrl\":\"clock\"},{\"title\":\"`M y clothing’s fine as velvet rare,Though under earth my dwel. lings a r e ;        And when above it I appear, M y enemies put me oft in fear.T h e gard’ner does at me repine, I spoil his works as he doesmine.`\",\"imageUrl\":\"../assets/images/mole.jpg\"},{\"title\":\"`My form is beauteous to the rav.        ish’d sight, My habit gay, my color gold or        white ; When ladies take the air, I       without pride,A faithful partner am close by        their side.   I near their persons constantly remain,        A favorite slave, bound with a  golden chain ;        And though I can both speak        and go alone,        Yet are my motions to myself        unknown.`\",\"imageUrl\":\"https://i7.pngguru.com/preview/911/45/206/clock-cartoon-clip-art-cartoon-vector-clock-png.jpg\"}]}";
+
+    static int item = 0;
+
+    TextView puzzleTitle;
+
+    ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        puzzleTitle = (TextView) findViewById(R.id.puzzle_title);
+
+        imageView = (ImageView) findViewById(R.id.imageUrl);
+
+        try{
+            JSONObject  jsonRootObject = new JSONObject(JSON_STRING);
+
+            //Get the instance of JSONArray that contains JSONObjects
+            JSONArray jsonArray = jsonRootObject.optJSONArray("result");
+            this.showCard(jsonArray.getJSONObject(item));
+
+//            for(int i=0; i < jsonArray.length(); i++){
+//                JSONObject jsonObject = jsonArray.getJSONObject(i);
+//
+//               // int id = Integer.parseInt(jsonObject.optString("id").toString());
+//                String name = jsonObject.optString("title").toString();
+//                String imageUrl = jsonObject.optString("imageUrl").toString();
+//
+//                System.out.println("name **"+name);;
+//                System.out.println("imageUrl **"+imageUrl);;
+//
+//            }
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
+        loadRewardedAd();
+
+        // Create the "retry" button, which tries to show a rewarded ad between game plays.
+        retryButton = findViewById(R.id.retry_button);
+        retryButton.setVisibility(View.INVISIBLE);
+        retryButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        startGame();
+                    }
+                });
+
+        // Create the "show" button, which shows a rewarded video if one is loaded.
+        showVideoButton = findViewById(R.id.show_video_button);
+        showVideoButton.setVisibility(View.INVISIBLE);
+        showVideoButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        showRewardedVideo();
+                    }
+                });
+
+        // Display current coin count to user.
+        coinCountText = findViewById(R.id.coin_count_text);
+        coinCount = 100;
+        coinCountText.setText("Coins: " + coinCount);
+
+        startGame();
+    }
+
+    private void showCard(JSONObject jsonObject) {
+
+        String title = jsonObject.optString("title").toString();
+        String imageUrl = jsonObject.optString("imageUrl").toString();
+        imageView.setImageResource(getResources().getIdentifier(imageUrl, "drawable", "com.christus.release1.admobApp"));
+        puzzleTitle.setText(title);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        pauseGame();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (!gameOver && gamePaused) {
+            resumeGame();
+        }
+    }
+
+    private void pauseGame() {
+        countDownTimer.cancel();
+        gamePaused = true;
+    }
+
+    private void resumeGame() {
+        createTimer(timeRemaining);
+        gamePaused = false;
+    }
+
+    private void loadRewardedAd() {
+        if (rewardedAd == null || !rewardedAd.isLoaded()) {
+            rewardedAd = new RewardedAd(this, AD_UNIT_ID);
+            isLoading = true;
+            rewardedAd.loadAd(
+                    new AdRequest.Builder().build(),
+                    new RewardedAdLoadCallback() {
+                        @Override
+                        public void onRewardedAdLoaded() {
+                            // Ad successfully loaded.
+                            MainActivity.this.isLoading = false;
+                            Toast.makeText(MainActivity.this, "onRewardedAdLoaded", Toast.LENGTH_SHORT).show();
+                        }
+
+                        @Override
+                        public void onRewardedAdFailedToLoad(int errorCode) {
+                            // Ad failed to load.
+                            MainActivity.this.isLoading = false;
+                            Toast.makeText(MainActivity.this, "onRewardedAdFailedToLoad", Toast.LENGTH_SHORT)
+                                    .show();
+                        }
+                    });
+        }
+    }
+
+    private void addCoins(int coins) {
+        coinCount += coins;
+        coinCountText.setText("Coins: " + coinCount);
+    }
+
+    private void startGame() {
+        // Hide the retry button, load the ad, and start the timer.
+        retryButton.setVisibility(View.INVISIBLE);
+        showVideoButton.setVisibility(View.INVISIBLE);
+        if (!rewardedAd.isLoaded() && !isLoading) {
+            loadRewardedAd();
+        }
+        createTimer(COUNTER_TIME);
+        gamePaused = false;
+        gameOver = false;
+    }
+
+    // Create the game timer, which counts down to the end of the level
+    // and shows the "retry" button.
+    private void createTimer(long time) {
+        final TextView textView = findViewById(R.id.timer);
+        if (countDownTimer != null) {
+            countDownTimer.cancel();
+        }
+        countDownTimer =
+                new CountDownTimer(time * 1000, 50) {
+                    @Override
+                    public void onTick(long millisUnitFinished) {
+                        timeRemaining = ((millisUnitFinished / 1000) + 1);
+                        textView.setText("seconds remaining: " + timeRemaining);
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        if (rewardedAd.isLoaded()) {
+                            showVideoButton.setVisibility(View.VISIBLE);
+                        }
+                        textView.setText("You Lose!");
+                        addCoins(GAME_OVER_REWARD);
+                        retryButton.setVisibility(View.VISIBLE);
+                        gameOver = true;
+                    }
+                };
+        countDownTimer.start();
+    }
+
+    private void showRewardedVideo() {
+        showVideoButton.setVisibility(View.INVISIBLE);
+        if (rewardedAd.isLoaded()) {
+            RewardedAdCallback adCallback =
+                    new RewardedAdCallback() {
+                        @Override
+                        public void onRewardedAdOpened() {
+                            // Ad opened.
+                            Toast.makeText(MainActivity.this, "onRewardedAdOpened", Toast.LENGTH_SHORT).show();
+                        }
+
+                        @Override
+                        public void onRewardedAdClosed() {
+                            // Ad closed.
+                            Toast.makeText(MainActivity.this, "onRewardedAdClosed", Toast.LENGTH_SHORT).show();
+                            // Preload the next video ad.
+                            MainActivity.this.loadRewardedAd();
+                        }
+
+                        @Override
+                        public void onUserEarnedReward(RewardItem rewardItem) {
+                            // User earned reward.
+                            Toast.makeText(MainActivity.this, "onUserEarnedReward", Toast.LENGTH_SHORT).show();
+                            addCoins(rewardItem.getAmount());
+                        }
+
+                        @Override
+                        public void onRewardedAdFailedToShow(int errorCode) {
+                            // Ad failed to display
+                            Toast.makeText(MainActivity.this, "onRewardedAdFailedToShow", Toast.LENGTH_SHORT)
+                                    .show();
+                        }
+                    };
+            rewardedAd.show(this, adCallback);
+        }
+    }
+}
